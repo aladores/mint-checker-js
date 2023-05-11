de146a3c6ec4dd2135a0c18732c5b9f9d943321c
angelolad <angelo.ladores@outlook.com>
2023-05-11 12:57:28 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+import { API_KEY } from './config.js';
+
+function handleSubmit() {
+  const searchBar = document.getElementById("search-bar");
+  const searchBarValue = searchBar.value;
+
+  //addr1q9ezzague67wzye6lpwvqs8swcr8y4xt87dml4zep99n4ffprcxk4qu56t4u8zhgtsry2l9ual69q9hygznf93v0vyfsh2gq2p
+  if (!searchBarValue.startsWith("addr")) {
+    console.log("Error: Address must begin with addr");
+    return;
+  }
+  window.location = "address.html?" + `addr=${searchBarValue}`;
+}
+
+function loadAddress() {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+
+  if (urlSearchParams.has("addr")) {
+    const formData = Object.fromEntries(urlSearchParams.entries());
+    //Update address header
+    const addressHeader = document.getElementById("address-header");
+    addressHeader.innerHTML = formData.addr;
+  }
+}
+
+function addEventListener() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    handleSubmit();
+  });
+}
+
+console.log("Hello");
+
+addEventListener();
+loadAddress();
