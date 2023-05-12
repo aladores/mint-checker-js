d3f4177065753d2d6ec22de1eb3a82e1b60ad9f5
angelolad <angelo.ladores@outlook.com>
2023-05-12 05:48:15 -0400

    Retrive mint transactions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 
 import { API_KEY } from './config.js';
+const ALL_TRANSACTION_STRING = "ALL TRANSACTIONS"
+const MINT_STRING = "MINT TRANSACTIONS"
 
 function handleSubmit() {
   const searchBar = document.getElementById("search-bar");
@@ -18,23 +20,68 @@ function loadAddress() {
 
   if (urlSearchParams.has("addr")) {
     const formData = Object.fromEntries(urlSearchParams.entries());
-    //Update address header
-    const addressHeader = document.getElementById("address-header");
-    addressHeader.innerHTML = formData.addr;
-
-    test();
+    updateDom(formData.addr);
   }
 }
 
-async function test() {
-  const url = "https://cardano-mainnet.blockfrost.io/api/v0/health"
-  console.log(API_KEY);
-  const response = await fetch(url, { headers: { 'project_id': `${API_KEY}` } })
-  const jsonData = await response.json();
-  console.log(jsonData);
+async function updateDom(address) {
+  //updateAddress(address);
+
+  //1. Get all transactions
+  const transactions = await getAllTransactions(address);
+  if (transactions.length === 0) {
+    console.log("No transactions found in this address.");
+    return;
+  }
+  //2. Find if any of those transactions are minted
+  const mintTransactions = await getAllMintTransactions(transactions);
+  //console.log(mintTransactions);
+
+  //3. Format transactions 
 }
 
-function addEventListener() {
+async function getAllTransactions(address) {
+  const jsonData = await fetchData(`https://cardano-mainnet.blockfrost.io/api/v0/addresses/${address}/transactions`, ALL_TRANSACTION_STRING);
+  const transactions = [];
+
+  for (let i = 0; i < jsonData.length; i++) {
+    transactions.push(jsonData[i].tx_hash);
+  }
+
+  return transactions;
+}
+
+async function getAllMintTransactions(transactions) {
+  const mintTransactions = [];
+  for (let i = 0; i < transactions.length; i++) {
+    const jsonData = await fetchData(`https://cardano-mainnet.blockfrost.io/api/v0/txs/${transactions[i]}`, MINT_STRING);
+    if (jsonData.asset_mint_or_burn_count === 1) {
+      mintTransactions.push(jsonData);
+    }
+  }
+  // mintTransactions.filter((item) => {
+  //   console.log(item);
+  // });
+  return mintTransactions;
+}
+
+async function fetchData(url, wantedData) {
+  console.log("Fetching: ", wantedData)
+  const response = await fetch(url, { headers: { 'project_id': `${API_KEY}` } })
+  if (response.status != 200) {
+    console.log(`Error fetching ${wantedData} transactions: `, response)
+    return;
+  }
+  const jsonData = await response.json();
+  return jsonData;
+}
+// function updateAddress(address) {
+//   //const addressHeader = document.getElementById("address-header");
+//   //addressHeader.innerHTML = address;
+//   //getMintTx(address)
+// }
+
+function addSearchEventListener() {
   const form = document.querySelector('form');
   form.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -42,7 +89,5 @@ function addEventListener() {
   });
 }
 
-console.log("Hello");
-
-addEventListener();
+addSearchEventListener();
 loadAddress();
