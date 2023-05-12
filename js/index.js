04c824f40751c034916c55c7cd6040f46452cef0
aladores <angelo.ladores@outlook.com>
2023-05-12 16:41:04 -0400

    Get asset name for minted unit

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@
 import { API_KEY } from './config.js';
 const ALL_TRANSACTION_STRING = "ALL TRANSACTIONS"
 const MINT_STRING = "MINT TRANSACTIONS"
-
+const ASSET_STRING = "SPECIFIC ASSET"
 function handleSubmit() {
   const searchBar = document.getElementById("search-bar");
   const searchBarValue = searchBar.value;
 
   //addr1q9ezzague67wzye6lpwvqs8swcr8y4xt87dml4zep99n4ffprcxk4qu56t4u8zhgtsry2l9ual69q9hygznf93v0vyfsh2gq2p
+  //addr1qydp5tjzex2xa87p8kz9hvm7e7mqj8wul28ylggvvrjf7n5sgq4a3gmwkf5ugalw6hgr8uvv4fk24mnl87hfcsx3pzjs3xkc73
   if (!searchBarValue.startsWith("addr")) {
     console.log("Error: Address must begin with addr");
     return;
@@ -30,14 +31,28 @@ async function updateDom(address) {
   //1. Get all transactions
   const transactions = await getAllTransactions(address);
   if (transactions.length === 0) {
+    //TO DO
     console.log("No transactions found in this address.");
     return;
   }
-  //2. Find if any of those transactions are minted
-  const mintTransactions = await getAllMintTransactions(transactions);
-  //console.log(mintTransactions);
 
-  //3. Format transactions 
+  //2. Find if any of those transactions are minted
+  //const testTx = ["88f5251bac8923cb70ae4826b2b491493ccf564fbb2f1384427fca68133f6786"];
+  //const mintTransactions = await getAllMintTransactions(testTx);
+  const mintTransactions = await getAllMintTransactions(transactions);
+  console.log(mintTransactions);
+
+  if (mintTransactions.length === 0) {
+    //TO DO
+    console.log("No mint transactions found in this address.");
+    return;
+  }
+
+  //3. Find and replace output amount
+  const mintTxWithName = await getSpecificAsset(mintTransactions);
+  console.log(mintTxWithName);
+  //4. Format transactions 
+
 }
 
 async function getAllTransactions(address) {
@@ -59,12 +74,30 @@ async function getAllMintTransactions(transactions) {
       mintTransactions.push(jsonData);
     }
   }
-  // mintTransactions.filter((item) => {
-  //   console.log(item);
-  // });
   return mintTransactions;
 }
+async function getSpecificAsset(mintTransactions) {
+  let assetName = "";
+  for (let i = 0; i < mintTransactions.length; i++) {
+    const param = mintTransactions[i].output_amount[1].unit;
+    const jsonData = await fetchData(`https://cardano-mainnet.blockfrost.io/api/v0/assets/${param}`, ASSET_STRING);
 
+    console.log(jsonData);
+    //For on chain metadata 
+    if (jsonData.onchain_metadata !== null) {
+      if (jsonData.onchain_metadata.hasOwnProperty("asset_ID")) {
+        assetName = jsonData.onchain_metadata["asset ID"];
+      }
+      else {
+        assetName = jsonData.onchain_metadata["name"];
+      }
+    }
+    console.log(assetName);
+    //Add asset name to mint transaction
+    mintTransactions[i].output_amount[1].name = assetName;
+  }
+  return mintTransactions;
+}
 async function fetchData(url, wantedData) {
   console.log("Fetching: ", wantedData)
   const response = await fetch(url, { headers: { 'project_id': `${API_KEY}` } })
