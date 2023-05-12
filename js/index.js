43bc397764aaa6cbfbfb207d669136809cc70af4
aladores <angelo.ladores@outlook.com>
2023-05-12 18:15:41 -0400

    Format dom with fetched data

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 import { API_KEY } from './config.js';
+import { MINT_TX_TEST } from './test.js';
 const ALL_TRANSACTION_STRING = "ALL TRANSACTIONS"
 const MINT_STRING = "MINT TRANSACTIONS"
 const ASSET_STRING = "SPECIFIC ASSET"
@@ -28,30 +29,36 @@ function loadAddress() {
 async function updateDom(address) {
   //updateAddress(address);
 
-  //1. Get all transactions
-  const transactions = await getAllTransactions(address);
-  if (transactions.length === 0) {
-    //TO DO
-    console.log("No transactions found in this address.");
-    return;
-  }
+  // //1. Get all transactions
+  // const transactions = await getAllTransactions(address);
+  // if (transactions.length === 0) {
+  //   //TO DO
+  //   console.log("No transactions found in this address.");
+  //   return;
+  // }
 
-  //2. Find if any of those transactions are minted
-  //const testTx = ["88f5251bac8923cb70ae4826b2b491493ccf564fbb2f1384427fca68133f6786"];
-  //const mintTransactions = await getAllMintTransactions(testTx);
-  const mintTransactions = await getAllMintTransactions(transactions);
-  console.log(mintTransactions);
+  // //2. Find if any of those transactions are minted
+  // //const testTx = ["88f5251bac8923cb70ae4826b2b491493ccf564fbb2f1384427fca68133f6786"];
+  // //const mintTransactions = await getAllMintTransactions(testTx);
+  // const mintTransactions = await getAllMintTransactions(transactions);
+  // console.log(mintTransactions);
 
-  if (mintTransactions.length === 0) {
-    //TO DO
-    console.log("No mint transactions found in this address.");
-    return;
-  }
+  // if (mintTransactions.length === 0) {
+  //   //TO DO
+  //   console.log("No mint transactions found in this address.");
+  //   return;
+  // }
 
   //3. Find and replace output amount
-  const mintTxWithName = await getSpecificAsset(mintTransactions);
-  console.log(mintTxWithName);
+  const mintTxWithName = MINT_TX_TEST;
+  //const mintTxWithName = await getSpecificAsset(mintTransactions);
+
+  updateAddressSection(address, mintTxWithName);//Replace with mintTransactions
+
   //4. Format transactions 
+  const formattedTransactions = formatTransactions(mintTxWithName);
+  console.log(formattedTransactions);
+  updateTransactionSection(formattedTransactions);
 
 }
 
@@ -92,12 +99,31 @@ async function getSpecificAsset(mintTransactions) {
         assetName = jsonData.onchain_metadata["name"];
       }
     }
-    console.log(assetName);
+
     //Add asset name to mint transaction
     mintTransactions[i].output_amount[1].name = assetName;
   }
   return mintTransactions;
 }
+
+function formatTransactions(transactions) {
+
+  const formattedTransactions = [];
+  for (let i = 0; i < transactions.length; i++) {
+
+    const { block_time, hash } = transactions[i];
+    const { name } = transactions[i].output_amount[1];
+
+    const formattedDate = new Date(block_time * 1000);
+    formattedTransactions.push({
+      block_time: formattedDate.toLocaleString(),
+      hash: hash,
+      name: name
+    });
+  }
+  return formattedTransactions;
+}
+
 async function fetchData(url, wantedData) {
   console.log("Fetching: ", wantedData)
   const response = await fetch(url, { headers: { 'project_id': `${API_KEY}` } })
@@ -108,11 +134,46 @@ async function fetchData(url, wantedData) {
   const jsonData = await response.json();
   return jsonData;
 }
-// function updateAddress(address) {
-//   //const addressHeader = document.getElementById("address-header");
-//   //addressHeader.innerHTML = address;
-//   //getMintTx(address)
-// }
+
+function updateAddressSection(address, transactions) {
+  const addressSection = document.getElementById("address-section");
+  const addressHeader = document.createElement("h2");
+  addressHeader.innerText = "Address";
+  addressHeader.classList.add("subheader-text");
+  addressSection.appendChild(addressHeader);
+
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("address");
+
+  newDiv.innerHTML = `
+    <p>${address}</p>
+    <p>Mint Transactions: ${transactions.length}</p>
+  `;
+
+  addressSection.appendChild(newDiv);
+
+}
+
+function updateTransactionSection(transactions) {
+  const transactionsSection = document.getElementById("transactions-section");
+  const transactionHeader = document.createElement("h2");
+  transactionHeader.innerText = "Transactions";
+  transactionHeader.classList.add("subheader-text");
+  transactionsSection.appendChild(transactionHeader);
+
+  for (let i = 0; i < transactions.length; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("transaction");
+
+    newDiv.innerHTML = `
+      <p>Transaction id: ${transactions[i].hash}</p>
+      <p>Received time: ${transactions[i].block_time}</p>
+      <p>Asset name: ${transactions[i].name}</p>
+    `;
+
+    transactionsSection.appendChild(newDiv);
+  }
+}
 
 function addSearchEventListener() {
   const form = document.querySelector('form');
---
+++ js/test.js
@@ -0,0 +1,104 @@
+export const MINT_TX_TEST = [
+    {
+        "hash": "617af3909dfbb282a41ad368fc6c3a280d37cada1c37bc02d11ae2ca81c8bcd8",
+        "block": "fca7130c6a067c2c0f50925c5416fcefcfed5eae787e4846f3629965933a5168",
+        "block_height": 7532567,
+        "block_time": 1658534592,
+        "slot": 66968301,
+        "index": 23,
+        "output_amount": [
+            {
+                "unit": "lovelace",
+                "quantity": "2594171"
+            },
+            {
+                "unit": "13b1e946ae6fd21e77a40de3005716a753d01fd69a70f389d0fb07b3436861696e734f6e446f6773353836",
+                "quantity": "1",
+                "name": "Fido Frenly"
+            }
+        ],
+        "fees": "405829",
+        "deposit": "0",
+        "size": 5519,
+        "invalid_before": null,
+        "invalid_hereafter": "97185093",
+        "utxo_count": 3,
+        "withdrawal_count": 0,
+        "mir_cert_count": 0,
+        "delegation_count": 0,
+        "stake_cert_count": 0,
+        "pool_update_count": 0,
+        "pool_retire_count": 0,
+        "asset_mint_or_burn_count": 1,
+        "redeemer_count": 0,
+        "valid_contract": true
+    },
+    {
+        "hash": "88f5251bac8923cb70ae4826b2b491493ccf564fbb2f1384427fca68133f6786",
+        "block": "e1279174afb6637ff6bf6a1082c2715e76e6b7912fd4d68ecfbd03bbcb5eef77",
+        "block_height": 7811590,
+        "block_time": 1664291188,
+        "slot": 72724897,
+        "index": 45,
+        "output_amount": [
+            {
+                "unit": "lovelace",
+                "quantity": "60825765"
+            },
+            {
+                "unit": "d5ac02157f1c4249f4671f397a3cd8424178054dca84c0ac8c2f404453454e42415a555255373330",
+                "quantity": "1",
+                "name": "LuckyPlanet Senbazuru #730"
+            }
+        ],
+        "fees": "217025",
+        "deposit": "0",
+        "size": 1400,
+        "invalid_before": null,
+        "invalid_hereafter": "72725422",
+        "utxo_count": 5,
+        "withdrawal_count": 0,
+        "mir_cert_count": 0,
+        "delegation_count": 0,
+        "stake_cert_count": 0,
+        "pool_update_count": 0,
+        "pool_retire_count": 0,
+        "asset_mint_or_burn_count": 1,
+        "redeemer_count": 0,
+        "valid_contract": true
+    },
+    {
+        "hash": "42dd21b5da64d193ae1e7f9a48f47d28abfea6929bef905429643cbced4524db",
+        "block": "c1b77b2f1ecd984a7055f18aed39ccb7364fb30e64610dd487c7d356e18ee149",
+        "block_height": 8093387,
+        "block_time": 1670095629,
+        "slot": 78529338,
+        "index": 12,
+        "output_amount": [
+            {
+                "unit": "lovelace",
+                "quantity": "2805635"
+            },
+            {
+                "unit": "74497be5db8eb78a206b54c094b5f718e568439983a708070508a04a447261676f6e7353323631",
+                "quantity": "1",
+                "name": "Thank you ❤️"
+            }
+        ],
+        "fees": "194365",
+        "deposit": "0",
+        "size": 882,
+        "invalid_before": null,
+        "invalid_hereafter": "78536482",
+        "utxo_count": 3,
+        "withdrawal_count": 0,
+        "mir_cert_count": 0,
+        "delegation_count": 0,
+        "stake_cert_count": 0,
+        "pool_update_count": 0,
+        "pool_retire_count": 0,
+        "asset_mint_or_burn_count": 1,
+        "redeemer_count": 0,
+        "valid_contract": true
+    }
+]
