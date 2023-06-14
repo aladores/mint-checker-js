ab4ef59b8660c3d55250a65c63c24f302e811a7f
aladores <angelo.ladores@outlook.com>
2023-06-14 09:04:23 -0400

    add node-fetch

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
+const fetch = require("node-fetch");
 
 require('dotenv').config();
 const port = process.env.PORT;
